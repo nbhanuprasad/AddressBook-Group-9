936a4f70dcb6503fa457f33a2ae74b9a15dde5ea
Venkatesh7660 <chowdaryvnktsh@gmail.com>
2021-10-20 23:19:31 -0400

    Restricting user specific data

--- addressbook/Contacts/views.py
+++ addressbook/Contacts/views.py
@@ -5,8 +5,8 @@ from .models import ContactInfo
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
 import os.path
 # Create your views here.
 #def home_view(request,*args, **kwargs):
@@ -19,23 +19,39 @@ class CustomLoginView(LoginView):
     def get_success_url(self):
         return reverse_lazy('contacts')
 
-class ContactList(ListView):
+class ContactList(LoginRequiredMixin,ListView):
     model= ContactInfo
     context_object_name= 'contacts'
-class ContactDetail(DetailView):
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['contacts']= context['contacts'].filter(user=self.request.user)
+        context['count']=context['contacts'].count()
+        return context
+class ContactDetail(LoginRequiredMixin,DetailView):
     model= ContactInfo
     context_object_name= 'contactdetail'
 
-class ContactCreate(CreateView):
+class ContactCreate(LoginRequiredMixin,CreateView):
     model= ContactInfo
-    fields = '__all__'
+    fields = ['fname','lname']
     success_url= reverse_lazy('contacts')
-class ContactUpdate(UpdateView):
+
+    def form_valid(self,form):
+        form.instance.user = self.request.user
+        return super(ContactCreate, self).form_valid(form)
+
+class ContactUpdate(LoginRequiredMixin,UpdateView):
     model= ContactInfo
     fields = '__all__'
     context_object_name='contact'
     success_url= reverse_lazy('contacts')
-class ContactDelete(DeleteView):
+
+    def form_valid(self,form):
+        form.instance.user = self.request.user
+        return super(ContactUpdate, self).form_valid(form)
+
+class ContactDelete(LoginRequiredMixin,DeleteView):
     model=ContactInfo
     context_object_name='delcontact'
     success_url= reverse_lazy('contacts')
